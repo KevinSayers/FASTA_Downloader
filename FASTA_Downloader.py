1c8d6025369a208d8d8ccbaebd4dcb80f8a42af6
Kevin Sayers <KevinSayers@users.noreply.github.com>
2015-01-12 16:11:33 -0700

    checked PEP8
    Checked formatting against PEP8 to make sure it was compliant

--- FASTA_Downloader.py
+++ FASTA_Downloader.py
@@ -1,5 +1,7 @@
 #! python2
+
 import urllib2
+
 
 def downloader(IdValue):
     UID = IdValue.strip()
@@ -8,20 +10,17 @@ def downloader(IdValue):
     FastaURL = FastaString1 + UID + FastaString2
     FastaPage = urllib2.urlopen(FastaURL)
     FastaData = FastaPage.read()
-    OutFile = open(UID+'.txt','w')
+    OutFile = open(UID+'.txt', 'w')
     OutFile.write(FastaData)
     OutFile.close()
 
 
 def main():
     InputFile = raw_input('File with list of UID values: ')
-    with open(InputFile,'r') as InputData:
+    with open(InputFile, 'r') as InputData:
         for i in InputData:
             print 'Downloading: ' + i
             downloader(i)
-    
-
 
 if __name__ == '__main__':
     main()
-    
